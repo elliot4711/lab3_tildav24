f4a989c4cc771538c125e472b372aabfdba00859
Elliot Stjernqvist <elliot@n137-p179.eduroam.kth.se>
2024-01-26 13:38:46 +0100

    Solved task 3

--- lab3.py
+++ lab3.py
@@ -8,3 +8,22 @@ with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
         else:
             svenska.put(ordet)             # in i sökträdet
 print("\n")
+
+engelska = Bintree()
+with open("engelska.txt", "r", encoding="utf-8") as engelskfil:
+    for rad in engelskfil:
+        ordlista = rad.split()
+        for ordet in ordlista:
+            ordet = ordet.replace('!', '')
+            ordet = ordet.replace('"', '')
+            ordet = ordet.replace(',', '')
+            ordet = ordet.replace('.', '')
+            ordet = ordet.replace("'", '')
+            if ordet in engelska:
+                pass
+            else:
+                engelska.put(ordet)
+                if ordet in svenska:
+                    print(ordet, end = " ") 
+                else:
+                    pass
